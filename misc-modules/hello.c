ee4de8b03cc26f91c6ac853865bfbea7f6f1bd23
Hyounjun Chang <hyounjun.chang@colorado.edu>
2026-02-28 02:08:50 -0700

    Minor change to hello.c

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,13 +7,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, this is Hyounjun's minor change\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, Hyounjun's kernel module has exited\n");
 }
 
 module_init(hello_init);
