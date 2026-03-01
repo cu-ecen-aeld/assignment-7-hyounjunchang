ee29adb4ea29e0e5b1dc18ea3403027cebbe1fdf
Hyounjun Chang <hyounjun.chang@colorado.edu>
2026-02-28 23:37:10 -0700

    printk github username instead

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,13 +7,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, this is Hyounjun's minor change\n");
+	printk(KERN_ALERT "Hello, my Github is hyounjunchang and this is a kernel module\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, Hyounjun's kernel module has exited\n");
+	printk(KERN_ALERT "Goodbye, my Github is hyounjunchang and this kernel module is exiting\n");
 }
 
 module_init(hello_init);
